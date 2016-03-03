3d7669c53fd07f2ee81ba6fe441b6eb1b3d88e08
Brian Haas <brianjohnhaas@gmail.com>
2016-03-03 11:17:30 -0500

    separating blast from promiscuity filter and adding tests

---
+++ test/test_promiscuity/cleanMe.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -f ./fusion_preds.txt.post_promisc_filter.info
+rm -f ./fusion_preds.txt.post_promisc_filter
---
+++ test/test_promiscuity/runMe.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+../../util/promiscuity_filter.pl  --fusion_preds fusion_preds.txt --genome_lib_dir $CTAT_GENOME_LIB
+

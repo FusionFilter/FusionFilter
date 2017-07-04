47ca2cd7a1adc12ff0789bba2fafccaf462c7e08
Brian Haas <brianjohnhaas@gmail.com>
2017-07-04 18:46:15 -0400

    works off gtf now

--- util/build_prot_info_db.pl
+++ util/build_prot_info_db.pl
@@ -7,22 +7,18 @@ use Getopt::Long qw(:config posix_default no_ignore_case bundling pass_through);
 use Data::Dumper;
 use JSON::XS;
 use FindBin;
-use lib ("$FindBin::Bin/../PerlLib");
-use Fasta_reader;
-use TiedHash;
+use lib ("$FindBin::Bin/../lib");
 use Process_cmd;
 
 my $usage = <<__EOUSAGE__;
 
 ##########################################################################
 #
-#  --ref_coding_GFF3 <string>        : reference annotation for coding genes in gff3 format
+#  --gtf <string>        : reference annotation for coding genes in gtf format
 #                                      
-#  --cds_fasta <string>              : CDS fasta file
+#  --genome_fa <string>       : genome fasta file
 #
-#  --pfam_domains <string>           : pfam output
-#
-#  --out_db_dir <string>             : directory to store prot_info.db.idx
+#  --out_db_dir <string>      : directory to store prot_info.db.idx
 #
 ##########################################################################
 
@@ -34,22 +30,19 @@ __EOUSAGE__
 
 
 my $help_flag;
-my $ref_coding_gff3_file;
-my $cds_fasta_file;
-my $pfam_domains_file;
+my $gtf_file;
+my $genome_fa;
 my $out_db_dir;
 
 &GetOptions ( 'h' => \$help_flag,
-              'ref_coding_GFF3=s' => \$ref_coding_gff3_file,
-              'cds_fasta=s' => \$cds_fasta_file,
-              'pfam_domains=s' => \$pfam_domains_file,
+              'gtf=s' => \$gtf_file,
+              'genome_fa=s' => \$genome_fa,
               'out_db_dir=s' => \$out_db_dir,
     );
 
-unless ($ref_coding_gff3_file && $cds_fasta_file && $pfam_domains_file && $out_db_dir) {
+unless ($gtf_file && $genome_fa && $out_db_dir) {
     die $usage;
 }
-
 
 
 main: {
@@ -61,111 +54,31 @@ main: {
     if (-e $prot_info_db_idx) {
         die "Error, $prot_info_db_idx already exists.  Please remove it or rename it before proceeding";
     }
-        
-    my $annot_manager = Annotation_manager->new($ref_coding_gff3_file);
 
-    print STDERR "-parsing cds fa: $cds_fasta_file\n";
-    my $fasta_reader = new Fasta_reader($cds_fasta_file);
-    my %cds_seqs = $fasta_reader->retrieve_all_seqs_hash();
-    
-    my %pfam_hits = &parse_pfam($pfam_domains_file);
-    
-    $annot_manager->add_cds_and_pfam(\%cds_seqs, \%pfam_hits);
-    
+    my $annot_manager = Annotation_manager->new($gtf_file, $genome_fa);
+
     $annot_manager->build_prot_info_db($prot_info_db_idx);
     
     
     exit(0);
 }
 
-
-=pfam_entry
-
-0       BTG
-1       PF07742.9
-2       116
-3       BTG2|ENST00000290551.4
-4       -
-5       158
-6       3.8e-42
-7       142.7
-8       0.0
-9       1
-10      1
-11      2.8e-46
-12      4.5e-42
-13      142.4
-14      0.0
-15      1
-16      115
-17      9
-18      121
-19      9
-20      122
-21      0.99
-22      BTG
-23      family
-
-=cut
-
-
-####
-sub parse_pfam {
-    my ($pfam_file) = @_;
-
-    print STDERR "-parsing $pfam_file\n";
-    
-    my %model_to_domains;
-    
-    open (my $fh, $pfam_file) or die "Error, cannot open file $pfam_file";
-    while (<$fh>) {
-        chomp;
-        unless (/\w/) { next; }
-        if (/^\#/) { next; }
-        my @x = split(/\s+/);
-        
-        if (scalar @x < 22) {
-            print STDERR "WARNING: Skipping line: $_ as likely corrupt.\n";
-            next;
-        }
-        
-        my $QueryProtID = $x[3];
-        my $pfam_id = $x[1];
-        my $HMMERDomain = $x[0];
-        my $HMMERTDomainDescription = join(" ", @x[22..$#x]);
-        my $QueryStartAlign = $x[17];
-        my $QueryEndAlign = $x[18];
-        my $PFAMStartAlign = $x[15];
-        my $PFAMEndAlign = $x[16];
-        my $FullSeqEvalue = $x[6];
-        my $ThisDomainEvalue = $x[11];
-        my $FullSeqScore = $x[7];
-        my $FullDomainScore = $x[13];
-        
-        my $pfam_domain_struct = { cds_id => $QueryProtID,
-                                   hmmer_domain => $HMMERDomain,
-                                   query_start => $QueryStartAlign,
-                                   query_end => $QueryEndAlign,
-                                   domain_evalue => $ThisDomainEvalue,
-        };
-        
-        push (@{$model_to_domains{$QueryProtID}}, $pfam_domain_struct);
-    }
-    close $fh;
-
-    return(%model_to_domains);
-}
-        
         
 
 #############################################################################
 package Annotation_manager;
 use strict;
 use warnings;
+use Gene_obj;
+use Gene_obj_indexer;
+use GTF_utils;
+use Fasta_reader;
+use TiedHash;
+use Carp;
 
 ####
 sub new {
-    my ($packagename, $gff3_file) = @_;
+    my ($packagename, $gtf_file, $genome_fa) = @_;
 
     my $self = {
         gene_to_CDS_features => {},  # gene_id -> cds_id -> cds_feature_obj
@@ -173,9 +86,7 @@ sub new {
     
     bless($self, $packagename);
 
-    $self->parse_GFF3_instantiate_featureset($gff3_file);
-
-    
+    $self->parse_GTF_instantiate_featureset($gtf_file, $genome_fa);
     
     return($self);
 }
@@ -196,7 +107,7 @@ sub get_gene_list {
 }
 
 ####
-sub get_CDS_features {
+sub get_CDS_features_for_gene {
     my $self = shift;
     my ($gene_id) = @_;
 
@@ -220,7 +131,7 @@ sub toString {
     my $text = "";
     
     foreach my $gene_id (@gene_ids) {
-        my @cds_features = $self->get_CDS_features($gene_id);
+        my @cds_features = $self->get_CDS_features_for_gene($gene_id);
         foreach my $cds_feature (@cds_features) {
             $text .= $cds_feature->toString() . "\n";
         }
@@ -238,7 +149,7 @@ sub add_cds_and_pfam {
     my @gene_ids = $self->get_gene_list();
     
     foreach my $gene_id (@gene_ids) {
-        my @cds_features = $self->get_CDS_features($gene_id);
+        my @cds_features = $self->get_CDS_features_for_gene($gene_id);
         foreach my $cds_feature (@cds_features) {
 
             my $cds_id = $cds_feature->{cds_id};
@@ -273,9 +184,8 @@ sub build_prot_info_db {
     my $coder = JSON::XS->new->convert_blessed;
     
     foreach my $gene_id (@gene_ids) {
-        my @cds_features = $self->get_CDS_features($gene_id);
+        my @cds_features = $self->get_CDS_features_for_gene($gene_id);
         
-
         my $gene_id_store = $gene_id;
         $gene_id_store =~ s/\|.*$//;
 
@@ -292,61 +202,86 @@ sub build_prot_info_db {
 
             
 ####
-sub parse_GFF3_instantiate_featureset {
+sub parse_GTF_instantiate_featureset {
     my ($self) = shift;
-    my ($gff3_file) = @_;
-    
-    my %mRNA_to_gene;
+    my ($gtf_file, $genome_fa) = @_;
 
-    print STDERR "-parsing gff3 coding regions file: $gff3_file\n";
-    open(my $fh, $gff3_file) or die "Error, cannot open file $gff3_file";
-    while (<$fh>) {
-        chomp;
-        unless (/\w/) { next; }
-        if (/^\#/) { next; }
-        
-        my @x = split(/\t/);
-        my $chr = $x[0];
-        
-        my %info = &_parse_info($x[8]);
+    my $gene_obj_indexer = {};
 
-        if ($x[2] eq 'mRNA') {
-            my $mRNA_id = $info{ID};
-            my $gene_id = $info{Parent};
-            $mRNA_to_gene{$mRNA_id} = $gene_id;
+    ## associate gene identifiers with contig id's.
+    &GTF_utils::index_GTF_gene_objs($gtf_file, $gene_obj_indexer);
+
+
+    my $fasta_reader = new Fasta_reader($genome_fa);
+    my %genome = $fasta_reader->retrieve_all_seqs_hash();
+
+    my @all_gene_ids = keys %$gene_obj_indexer;
+    my %contig_to_gene_list;
+    foreach my $gene_id (@all_gene_ids) {
+        my $gene_obj = $gene_obj_indexer->{$gene_id};
+
+        my $contig = $gene_obj->{asmbl_id}
+        or croak "Error, can't find contig id attached to gene_obj($gene_id) as asmbl_id val\n"
+            . $gene_obj->toString();
+
+
+        my $gene_list_aref = $contig_to_gene_list{$contig};
+        unless (ref $gene_list_aref) {
+            $gene_list_aref = $contig_to_gene_list{$contig} = [];
         }
-        elsif ($x[2] eq 'CDS') {
-            my $cds_id = $info{ID};
-            my $mRNA_id = $info{Parent};
-            my $gene_id = $mRNA_to_gene{$mRNA_id} or die "Error, no gene_id for mRNA: $mRNA_id";
-            
-            my $start = $x[3];
-            my $end = $x[4];
-            my $orient = $x[6];
-            my $phase_init = $x[7];
-            
-            $phase_init = &_phase_transform($phase_init);
-            
-            $self->_add_to_CDS_feature($gene_id, $mRNA_id, $chr, $start, $end, $orient, $phase_init);
-        }
+
+        push (@$gene_list_aref, $gene_id);
+
     }
 
-    $self->_refine_CDS_features();
+    foreach my $asmbl_id (sort keys %contig_to_gene_list) {
+
+        my $genome_seq = $genome{$asmbl_id} or croak "Error, no sequence stored for contig: $asmbl_id";
+
+        my @gene_ids = @{$contig_to_gene_list{$asmbl_id}};
+
+        foreach my $gene_id (@gene_ids) {
+            my $gene_obj_ref = $gene_obj_indexer->{$gene_id};
+            
+            $gene_obj_ref->create_all_sequence_types(\$genome_seq);
+            
+            foreach my $isoform ($gene_obj_ref, $gene_obj_ref->get_additional_isoforms()) {
+
+                unless ($isoform->is_coding_gene()) { next; }
+
+                $isoform->set_CDS_phases(\$genome_seq);
+                
+                my $isoform_id = $isoform->{Model_feat_name};
+                my $gene_id = $isoform->{TU_feat_name};
+                my $orient = $isoform->get_orientation();
+                
+                my $cds_seq = $isoform->get_CDS_sequence();
+
+                my $cds_feature_obj = $self->get_CDS_feature($gene_id, $isoform_id);
+                
+                my @exons = $isoform->get_exons();
+                foreach my $exon (@exons) {
+                    if (my $cds_obj = $exon->get_CDS_obj()) {
+                        
+                        my ($cds_lend, $cds_rend) = sort {$a<=>$b} $cds_obj->get_coords();
+                        my $phase = $cds_obj->get_phase();
+                        
+                        $cds_feature_obj->add_segment($asmbl_id, $cds_lend, $cds_rend, $orient, $phase);
+                    }
+                }
+                
+                $cds_feature_obj->set_CDS_sequence($cds_seq);
+                $cds_feature_obj->refine();
+            }
+        }
+    }
+    
+    return;
 }
 
-####
-sub _add_to_CDS_feature {
-    my ($self) = shift;
-    my ($gene_id, $cds_id, $chr, $start, $end, $orient, $phase_init) = @_;
-
-    my $cds_feature_obj = $self->_get_CDS_feature($gene_id, $cds_id);
-    
-    $cds_feature_obj->add_segment($chr, $start, $end, $orient, $phase_init);
-    
-}
 
 ####
-sub _get_CDS_feature {
+sub get_CDS_feature {
     my ($self) = shift;
     my ($gene_id, $cds_id) = @_;
 
@@ -357,41 +292,6 @@ sub _get_CDS_feature {
     }
 
     return($cds_feature_obj);
-}
-
-
-####
-sub _phase_transform {
-    my ($phase) = @_;
-
-    if ($phase eq "0") {
-        return(0);
-    }
-    elsif ($phase eq "1") {
-        return(2);
-    }
-    elsif ($phase eq "2") {
-        return(1);
-    }
-    else {
-        return($phase);
-    }
-}
-
-    
-####
-sub _parse_info {
-    my ($info) = @_;
-
-    my %ret;
-
-    my @keyvals = split(/;/, $info);
-    foreach my $keyval (@keyvals) {
-        my ($key, $val) = split(/=/, $keyval);
-        $ret{$key} = $val;
-    }
-    
-    return(%ret);
 }
 
 
@@ -451,6 +351,8 @@ sub new {
 sub TO_JSON {
     return { %{ shift() } };
 }
+
+
 
 
 ####
@@ -572,12 +474,3 @@ sub set_CDS_sequence {
     return;
 }
 
-####
-sub add_pfam_hits {
-    my $self = shift;
-    my (@pfam_hits) = @_;
-
-    push (@{$self->{pfam_hits}}, @pfam_hits);
-
-    return;
-}

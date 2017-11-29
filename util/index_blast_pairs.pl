34b3bbc49441f6e4d77cf858dea80afe3ba698c1
Brian Haas <brianjohnhaas@gmail.com>
2017-11-29 08:58:57 -0500

    store single best blast hit, allow multiple search files

--- util/index_blast_pairs.pl
+++ util/index_blast_pairs.pl
@@ -6,28 +6,52 @@ use FindBin;
 use lib ("$FindBin::Bin/../lib");
 use TiedHash;
 use Carp;
+use PerlIO::gzip;
 
+my $usage = "\n\n\tusage: output_index_filename blast_pairs.gene_sym.gz [blast_pairs_supp.gene_sym.gz ...]\n\n";
 
-my $usage = "\n\n\tusage: $0 blastn.gene_pairs.gz output_index_filename\n\n";
+my $output_index_filename = $ARGV[0] or die $usage;
+shift @ARGV;
+my @blast_pairs_files = @ARGV;
 
-my $blast_pairs = $ARGV[0] or die $usage;
-my $output_index_filename = $ARGV[1] or die $usage;
+unless (@blast_pairs_files) {
+    die $usage;
+}
+
 
 main: {
 
     my $idx = new TiedHash( { create => $output_index_filename } );
+
+    my %best_bit_score;
     
-    open (my $fh, "gunzip -c $blast_pairs | ") or die "Error, cannot read $blast_pairs  ";
-    while (<$fh>) {
-        chomp;
-        my ($geneA, $geneB, $per_id, $Evalue) = split(/\s+/);
-        if ($geneA eq $geneB) { next; }
+    foreach my $blast_pairs (@blast_pairs_files) {
+
+        print STDERR "-processing blast pairs from: $blast_pairs\n";
         
-        my $token = "{$geneA|$geneB|pID:$per_id|E:$Evalue}";
+        open (my $fh, "<:gzip", $blast_pairs) or die "Error, cannot read $blast_pairs  ";
         
-        $idx->store_key_value("$geneA--$geneB", $token);
-        $idx->store_key_value("$geneB--$geneA", $token);
+        while (<$fh>) {
+            chomp;
+            my @x = split(/\t/);
+            my ($geneA, $geneB, $per_id, $Evalue, $score) = ($x[0], $x[1], $x[2], $x[10], $x[11]);
                 
+            if ($geneA eq $geneB) { next; }
+            
+            my $gene_pair_tok = join("^", sort ($geneA, $geneB));
+            
+            if ( (! exists $best_bit_score{$gene_pair_tok}) || $best_bit_score{$gene_pair_tok} < $score) {
+
+                $best_bit_score{$gene_pair_tok} = $score;
+                
+            
+                my $token = "{$geneA|$geneB|pID:$per_id|E:$Evalue}";
+                
+                $idx->store_key_value("$geneA--$geneB", $token);
+                $idx->store_key_value("$geneB--$geneA", $token);
+                
+            }
+        }
     }
     
     print STDERR "-done creating index file: $output_index_filename\n\n";

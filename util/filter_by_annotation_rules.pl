f12f5d2b2c166c545199e257a5aff184922fe208
Brian Haas <brianjohnhaas@gmail.com>
2018-02-20 20:37:15 -0500

    update usage, add min FFPM filter

--- util/filter_by_annotation_rules.pl
+++ util/filter_by_annotation_rules.pl
@@ -8,11 +8,54 @@ use FindBin;
 use lib ("$FindBin::Bin/../lib");
 use DelimParser;
 
+use Getopt::Long qw(:config posix_default no_ignore_case bundling pass_through);
 
-my $usage = "usage: $0 CTAT.fusion_predictions.tsv ctat_genome_lib_dir\n\n";
+my $help_flag;
 
-my $predictions_file = $ARGV[0] or die $usage;
-my $genome_lib_dir = $ARGV[1] or die $usage;
+
+my $usage = <<__EOUSAGE__;
+
+#############################################################
+#
+#  --fusions <string>    fusion predictions
+#
+#  --genome_lib_dir <string>   CTAT genome lib dir
+#
+# Optional:
+#
+#  --min_FFPM <float>     min FFPM 
+#
+#
+###########################################################
+
+__EOUSAGE__
+
+    ;
+
+
+my $predictions_file;
+my $genome_lib_dir;
+my $min_FFPM;
+
+&GetOptions ( 'h' => \$help_flag,
+
+              'fusions=s' => \$predictions_file,
+              'genome_lib_dir=s' => \$genome_lib_dir,
+   
+              'min_FFPM=f' => \$min_FFPM,
+    );
+
+
+unless ($genome_lib_dir) {
+    if (exists $ENV{CTAT_GENOME_LIB}) {
+        $genome_lib_dir = $ENV{CTAT_GENOME_LIB};
+    }
+}
+
+unless ($predictions_file && $genome_lib_dir) {
+    die $usage;
+}
+
 
 main: {
     
@@ -47,11 +90,25 @@ main: {
         if (my $filter_reason = &AnnotFilterRule::examine_fusion_prediction($pred)) {
             $pred->{$fail_reason_header} = $filter_reason;
             $fail_writer->write_row($pred);
+            next;
         }
-        else {
-            # all good
-            $pass_writer->write_row($pred);
+
+        if (defined($min_FFPM)) {
+            my $ffpm = $pred->{FFPM};
+            unless (defined $ffpm) {
+                confess "Error, no FFPM value defined in file: $predictions_file, row: " . Dumper($pred);
+            }
+            if ($ffpm < $min_FFPM) {
+                $pred->{$fail_reason_header} = " FFPM $ffpm < min_FFPM required of $min_FFPM ";
+                $fail_writer->write_row($pred);
+                next;
+            }
         }
+        
+        
+        # all good
+        $pass_writer->write_row($pred);
+        
     }
 
 

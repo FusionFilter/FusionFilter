0af38b4a84f59983b95709ee839f77d191f3b4a6
Brian Haas <brianjohnhaas@gmail.com>
2018-02-28 13:49:52 -0500

    update to test to reflect updated behavior

--- testing/test_promiscuity/cleanMe.sh
+++ testing/test_promiscuity/cleanMe.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 rm -f ./fusion_preds.txt.post_promisc_filter.info
-rm -f ./fusion_preds.txt.post_promisc_filter
+rm -f ./fusion_preds.txt.post_promisc_filte
+rm -f ./fusion_preds.txt.post_promisc_filter.sorted
+
--- testing/test_promiscuity/runMe.sh
+++ testing/test_promiscuity/runMe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-../../util/promiscuity_filter.pl  --fusion_preds fusion_preds.txt --genome_lib_dir $CTAT_GENOME_LIB --exclude_loci_overlap_check
+../../util/promiscuity_filter.pl  --fusion_preds fusion_preds.txt --genome_lib_dir $CTAT_GENOME_LIB --exclude_loci_overlap_check --max_promiscuity 3
 

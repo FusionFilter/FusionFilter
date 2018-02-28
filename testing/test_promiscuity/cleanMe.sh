6b200b416b12b78addc1c79aaa5ce98686f57179
Brian Haas <brianjohnhaas@gmail.com>
2018-02-28 14:05:53 -0500

    update

--- testing/test_promiscuity/cleanMe.sh
+++ testing/test_promiscuity/cleanMe.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 rm -f ./fusion_preds.txt.post_promisc_filter.info
-rm -f ./fusion_preds.txt.post_promisc_filte
 rm -f ./fusion_preds.txt.post_promisc_filter.sorted
-
+rm -f ./fusion_preds.txt.post_promisc_filter
